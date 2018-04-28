88544692238c2f908fd239731568c6a499946496
Gabriel Tadiello Moraes <gtmoraes@inf.ufrgs.br>
2018-04-28 15:42:08 -0300

    Adding removeFromBlocked(), getThreadWaitingFor() and cjoin_struct to cutils.h

--- include/cutils.h
+++ include/cutils.h
@@ -20,6 +20,14 @@ Autores: Gabriel Tadiello, Laura Soares e Leonardo Bombardelli
 //For the meanwhile, i'm using this code as a guideline for the use of the ucontext primitives
 //https://gist.github.com/DanGe42/7148946
 
+
+// Struct containing the TID of a thread that's waiting for another (and the TID from the other thread)
+typedef struct cjs{
+  int waitingTID;
+  int beingWaitedTID;
+} cjoin_struct;
+
+
 //Queues for each state a thread can be in:
 extern FILA2 readyQueue;
 extern FILA2 blockedQueue;
@@ -43,6 +51,15 @@ int scheduler();
 void endExecScheduler();
 
 int InitializingCThreads();
+
+// Returns the TID of the thread that was waiting for the thread with TID "beingWaitedTID" (if there is one)
+// If finds a thread waiting for the "beingWaitedTID", it also removes the cjoin_struct from the queue
+// If there's no thread waiting for it, *ok == 0 (*ok == 1 otherwise)
+int getThreadWaitingFor(int beingWaitedTID, int *ok)
+
+// Removes a thread from the suspendedQueue and insert it to the readyQueue
+// Returns 0 upon success, -1 otherwise (probably won't be neccessary tho)
+int removeFromBlocked(int tid)
 
 
 #endif
